76e0b05ae8ba1034226d5c74f06205b17436a8b7
HelisonOliveira <helisonoliveira@hotmail.com>
2022-06-21 16:46:53 -0300

    planejando a execução do projeto

--- calculadoraSalario.js
+++ calculadoraSalario.js
@@ -1,4 +1,14 @@
+// fazer um código q quando possa adicionar 3 valores no inicio (valor, horas e imposto)
+// imprimir o valor do salario final do mês
+// 40hs semanais
+// 60% na hora extra
 
-let salario = 1500
+// criar função para cálculo do imposto
+// criar função para cálculo das horas extras
+// imprimir resultado
+
+let salario = 2000
 let imposto = 0.27
 let horas = 40
+
+
