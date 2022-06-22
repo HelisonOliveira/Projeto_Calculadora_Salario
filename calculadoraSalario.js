79b70ffbefc753efdc2ec707b15b0713a7702967
HelisonOliveira <helisonoliveira@hotmail.com>
2022-06-21 21:57:14 -0300

    Projeto Finalizado

--- calculadoraSalario.js
+++ calculadoraSalario.js
@@ -11,16 +11,14 @@
 let salario = 2000
 let imposto = 0.27
 let horas = 44
-//console.log(calcularHoraExtra(horas))
+
+console.log(imprimirSalarioMes(salario, imposto, horas))
 
 function calcHoraNormal (salario){
         let horasSemanais = 40
-      return  salario/horasSemanais
-               
+      return  salario/horasSemanais              
 } 
-
- vrHrNormal = calcHoraNormal (salario)
-console.log(calcHoraNormal(salario))
+//console.log(calcHoraNormal(salario))
 
 function verificarQuantHrExtra (horas) {
     let horaExtra = 0
@@ -30,28 +28,34 @@ function verificarQuantHrExtra (horas) {
         return horaExtra 
     } return horaExtra
 }
+//console.log(verificarQuantHrExtra(horas))
 
-
-console.log(verificarQuantHrExtra(horas))
-let quantHrExtra = verificarQuantHrExtra(horas)
-
-
-function calcularHoraExtra (quantHrExtra) {
-    
+function calcularHoraExtra (horas) {
+    let quantHrExtra = verificarQuantHrExtra(horas)
     let acrescimoHoraExtra = 0.6
     let horaNormal = calcHoraNormal(salario)
-    console.log(horaNormal*(1+acrescimoHoraExtra))
     let vrHrExtra = 0
         if (quantHrExtra > 0) {
-              vrHrExtra =  quantHrExtra * (horaNormal*(1+acrescimoHoraExtra))
-              
-        return vrHrExtra 
+            vrHrExtra =  quantHrExtra * (horaNormal*(1+acrescimoHoraExtra))  
+            return vrHrExtra 
         } return vrHrExtra 
     }
-console.log(calcularHoraExtra(quantHrExtra))
 
-// 
+//console.log(calcularHoraExtra(quantHrExtra))
 
-// function calcularImposto () {
-//     valorImposto = 
-// }
+function calcularImposto (salario) {
+        let valorHrExtra = calcularHoraExtra(horas)
+        let vrTotalSalario = salario + valorHrExtra
+        let valorImposto = vrTotalSalario*imposto
+    return  valorImposto
+}
+//console.log(calcularImposto(salario))
+
+function imprimirSalarioMes (salario, imposto, horas) {
+    let valorHrExtra = calcularHoraExtra(horas)
+    let vrImposto = calcularImposto(salario)
+    let salarioBruto = salario + valorHrExtra
+    let salarioLiquido = salarioBruto - vrImposto
+    
+   return `O Valor a receber é de: Salário Bruto R$: ${salarioBruto.toFixed(2).toString().replace(".",",")} {(+)Salário: R$ ${salario.toFixed(2).toString().replace(".",",")} (+)Horas Extras: R$ ${valorHrExtra.toFixed(2).toString().replace(".",",")}} (-) {Imposto: R$ ${vrImposto.toFixed(2).toString().replace(".",",")}} = Salário Líquido a receber é de R$ : ${salarioLiquido.toFixed(2).toString().replace(".",",")} `
+}
